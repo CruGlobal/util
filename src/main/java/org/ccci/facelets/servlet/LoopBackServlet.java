eb6b47a7ef0a2d2cc2bbfa52cec6ec9bb224a540
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2009-09-24 00:53:49 +0000

    remove unnecessary stuff
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@5644 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/facelets/servlet/LoopBackServlet.java
+++ src/main/java/org/ccci/facelets/servlet/LoopBackServlet.java
@@ -2,7 +2,6 @@ package org.ccci.facelets.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -13,20 +12,7 @@ import org.ccci.facelets.navigation.RequestController;
  
 public class LoopBackServlet extends HttpServlet {
 	private static final long serialVersionUID = 6L;
-	 
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-	}
- 
-	protected void service(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		super.service(request, response);
-	}
- 
-	public void destroy() {
-		super.destroy(); 
-	}
- 
+
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// retrieve the request controller from the session
