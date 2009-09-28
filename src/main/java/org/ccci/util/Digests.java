cd969c9b4df5be96232725ba034531d402031523
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2009-09-28 14:11:52 +0000

    fuller docs,  exception messages more accurate
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@5651 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/Digests.java
+++ src/main/java/org/ccci/util/Digests.java
@@ -4,8 +4,16 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * A utility class to make hash/digest algorithms easy to use.
+ * 
+ * @author Matt Drees
+ *
+ */
 public class Digests
 {
+    
+    private Digests() {} //uninstantiable
 
     /**
      * Returns a hex string representing the SHA-1 digest of the given message
@@ -24,7 +32,7 @@ public class Digests
         }
         catch (NoSuchAlgorithmException e)
         {
-            throw new AssertionError("SHA-1 algorithm should be part of JRE");
+            throw new IllegalStateException("SHA-1 is unavailable.  It is built into Sun's cryptograpy provider; is a different provider being used here?");
         }
         byte[] messageAsBytes;
         try
@@ -33,7 +41,7 @@ public class Digests
         }
         catch (UnsupportedEncodingException e)
         {
-            throw new AssertionError("UTF-8 encoding should be part of JRE");
+            throw new AssertionError("UTF-8 encoding should be part of a compliant JRE");
         }
         md.update(messageAsBytes);
         byte[] digest = md.digest();
