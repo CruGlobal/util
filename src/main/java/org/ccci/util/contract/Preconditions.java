14cf94ded67146a0436598574b91b063fc7ec65c
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2009-10-12 19:35:26 +0000

    improve docs, refactor slightly
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@5746 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/contract/Preconditions.java
+++ src/main/java/org/ccci/util/contract/Preconditions.java
@@ -1,16 +1,49 @@
 package org.ccci.util.contract;
 
+/**
+ * A set of precondition methods.  These are all are intended to be used to check parameters.  For internal-state-checking methods, you could
+ * use Google Collection's Preconditions class.
+ * 
+ * See {@link #checkArgument(boolean, String, Object...)} for typical usage example.
+ * 
+ * All parameters to each method are required.
+ * 
+ * @author Matt Drees
+ *
+ */
 public class Preconditions
 {
 
+    /**
+     * Verifies that the given reference is not null. If the given string is null, an {@link NullPointerException}
+     * is thrown with an exception message generated from calling {@link String#format(String, Object...)} on the
+     * given {@code message} and {@code parameters}.
+     * 
+     * @param string the string to check
+     * @param message a format string
+     * @param parameters parameter values for the format string
+     * @return the given {@code reference} to make inline checks possible.
+     */
     public static <T> T checkNotNull(T reference, String message, Object... parameters)
     {
         if (reference == null) throw new NullPointerException(String.format(message, parameters));
         return reference;
     }
 
+
+    /**
+     * Verifies that the given string is not null and is not an empty string.
+     * If the given string is null, a {@link NullPointerException} is thrown.
+     * If the given string is empty, an {@link IllegalArgumentException} is thrown.  
+     * In either case, an appropriate exception message based on {@code referenceName} is generated.
+     * 
+     * @param string the string to check
+     * @param referenceName should refer to the name of the parameter being checked
+     * @return the given string to make inline checks possible.
+     */
     public static String checkNotNullOrEmpty(String string, String referenceName)
     {
+        checkReferenceName(referenceName);
         checkNotNull(string, "%s is null", referenceName);
         if (string.isEmpty()) illegalArg("%s is empty", referenceName);
         return string;
@@ -21,22 +54,81 @@ public class Preconditions
         throw new IllegalArgumentException(String.format(message, parameters));
     }
 
-    public static void checkSize(String string, int minLength, int maxSize, String referenceName)
+
+    /**
+     * A shortcut for performing both {@link #checkArgumentMaxLength(String, int, String)} and {@link #checkArgumentMaxLength(String, int, String)}. 
+     * See those docs for details.
+     * 
+     * @param string the string to check
+     * @param minLength
+     * @param maxLength
+     * @param referenceName should refer to the name of the parameter being checked
+     * @return the given {@code string} to make inline checks possible
+     */
+    public static String checkArgumentSize(String string, int minLength, int maxLength, String referenceName)
     {
-        checkMinSize(string, minLength, referenceName);
-        checkMaxSize(string, maxSize, referenceName);
+        checkArgumentMinLength(string, minLength, referenceName);
+        checkArgumentMaxLength(string, maxLength, referenceName);
+        return string;
     }
 
-    public static void checkMinSize(String string, int minLength, String referenceName)
+    /**
+     * Verifies that the given string is at least {@code minLength} characters long.
+     * If the given string is too short, an {@link IllegalArgumentException} is thrown whose message is based on {@code referenceName}.
+     * 
+     * @param string the string to check
+     * @param minLength 
+     * @param referenceName should refer to the name of the parameter being checked
+     * @return the given {@code string} to make inline checks possible
+     */
+    public static String checkArgumentMinLength(String string, int minLength, String referenceName)
     {
-        if (string.length() < minLength) illegalArg("%s has less than %s character(s)", referenceName, minLength);
+        checkReferenceName(referenceName);
+        if (string.length() < minLength) illegalArg("%s has less than %s character%s (it has %s characters)", referenceName, minLength, minLength == 1 ? "" : "s", string.length());
+        return string;
+    }
+
+
+    private static void checkReferenceName(String referenceName)
+    {
+        checkNotNull(referenceName, "Invalid use of Preconditions method: referenceName is null");
     }
     
-    public static void checkMaxSize(String string, int maxSize, String referenceName)
+    /**
+     * Verifies that the given string is at most {@code maxLength} characters long.
+     * If the given string is too long, an {@link IllegalArgumentException} is thrown whose message is based on {@code referenceName}.
+     * 
+     * @param string the string to check
+     * @param maxLength 
+     * @param referenceName should refer to the name of the parameter being checked
+     * @return the given {@code string} to make inline checks possible
+     */
+    public static String checkArgumentMaxLength(String string, int maxSize, String referenceName)
     {
-        if (string.length() > maxSize) illegalArg("%s has more than %s character(s)", referenceName, maxSize);
+        checkReferenceName(referenceName);
+        if (string.length() > maxSize) illegalArg("%s has more than %s character%s (it has %s characters)", referenceName, maxSize, maxSize == 1 ? "" : "s", string.length());
+        return string;
     }
 
+    /**
+     * Verifies that the given condition is true. If the given condition is not true, an
+     * {@link IllegalArgumentException} is thrown whose message is the result of calling
+     * {@link String#format(String, Object...)} on the given {@code message} and {@code parameters}. Calling code
+     * should be used like this:
+     * 
+     * <pre>
+     * public void doSomething(Foo foo)
+     * {
+     *    Preconditions.checkArgument(foo.isInitialized, &quot;foo %s is not initialized!&quot;, foo);
+     *    ...
+     * }
+     * </pre>
+     * 
+     * 
+     * @param string the string to check
+     * @param maxLength
+     * @param referenceName should refer to the name of the parameter being checked
+     */
     public static void checkArgument(boolean condition, String message, Object... parameters)
     {
         if (!condition) illegalArg(message, parameters);
@@ -44,12 +136,14 @@ public class Preconditions
 
     /**
      * Verifies the given string can be used as a valid <a href="contract of http://www.w3.org/TR/xmlschema-2/#token">token</a>
-     * @param string
-     * @param referenceName
-     * @return
+     * 
+     * @param string the string to check
+     * @param referenceName should refer to the name of the parameter being checked
+     * @return the given {@code string}, to make inline checks possible
      */
     public static String checkNonEmptyToken(String string, String referenceName)
     {
+        checkReferenceName(referenceName);
         checkNotNullOrEmpty(string, referenceName);
         if (string.startsWith(" ")) illegalArg("%s starts with a space", string);
         if (string.endsWith(" ")) illegalArg("%s ends with a space", string);
@@ -58,6 +152,13 @@ public class Preconditions
         if (string.contains("\n")) illegalArg("%s contains a newline character", string);
         if (string.contains("\r")) illegalArg("%s contains a carriage return character", string);
         return string;
+    }
+
+
+    public static void checkState(boolean condition, String message, Object... parameters)
+    {
+        checkNotNull(message, "Invalid use of Preconditions.checkState(): message is null");
+        if (!condition) throw new IllegalStateException(String.format(message, parameters));
     }
 
 
