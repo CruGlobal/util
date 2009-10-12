cf4c5dcb7efc7bbe6e6fba5a877c0c1f52a68d6d
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2009-10-12 18:56:39 +0000

    add Base64 encoding utility
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@5744 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/Bytes.java
+++ src/main/java/org/ccci/util/Bytes.java
@@ -1,6 +1,7 @@
 package org.ccci.util;
 
 import org.ccci.util.contract.Preconditions;
+import org.jboss.seam.util.Base64;
 
 public class Bytes
 {
@@ -10,21 +11,20 @@ public class Bytes
      * 
      * Fast convert a byte array to a hex string with possible leading zero.
      * 
-     * @param b
-     *            array of bytes to convert to string
+     * @param bytes array of bytes to convert to string
      * @return hex representation, two chars per byte.
      */
-    public static String toHexString(byte[] b)
+    public static String toHexString(byte[] bytes)
     {
-        Preconditions.checkNotNull(b, "b is null");
-        StringBuilder sb = new StringBuilder(b.length * 2);
-        for (int i = 0; i < b.length; i++)
+        Preconditions.checkNotNull(bytes, "bytes is null");
+        StringBuilder sb = new StringBuilder(bytes.length * 2);
+        for (int i = 0; i < bytes.length; i++)
         {
             // look up high nibble char
-            sb.append(hexChar[(b[i] & 0xf0) >>> 4]);
+            sb.append(hexChar[(bytes[i] & 0xf0) >>> 4]);
 
             // look up low nibble char
-            sb.append(hexChar[b[i] & 0x0f]);
+            sb.append(hexChar[bytes[i] & 0x0f]);
         }
         return sb.toString();
     }
@@ -34,4 +34,15 @@ public class Bytes
      */
     static final char[] hexChar = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
+    /**
+     * Return a Base64 representation of the given byte string.
+     * See http://en.wikipedia.org/wiki/Base64 for info on Base64.
+     * @param bytes array of bytes to encode using Base64 encoding
+     */
+    public static String toBase64String(byte[] bytes)
+    {
+        Preconditions.checkNotNull(bytes, "bytes is null");
+        //for now, use Base64 util inside Seam.  May in the future want to not depend on Seam for this.
+        return Base64.encodeBytes(bytes);
+    }
 }
