cfec03cfb0f5916160d8fdb51265ed90f5f2b5df
travis.garrison@uscm.org <travis.garrison@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2009-10-23 15:06:14 +0000

    Added Pattern NUMERIC
    JIRA CCP-38
    
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@5803 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/Patterns.java
+++ src/main/java/org/ccci/util/Patterns.java
@@ -3,6 +3,7 @@ package org.ccci.util;
 public class Patterns
 {
 
-    public static final String ALPHANUMERIC = "[a-zA-Z0-9]*";
+    public static final String ALPHANUMERIC	= "[a-zA-Z0-9]*";
+    public static final String NUMERIC 		= "[0-9]*";
 
 }
