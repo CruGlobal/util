53abbb1bfbd19001c3431f6a5d4b805487dd3533
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-01-23 00:44:23 +0000

    add numeric pattern
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@6093 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/Patterns.java
+++ src/main/java/org/ccci/util/Patterns.java
@@ -5,5 +5,6 @@ public class Patterns
 
     public static final String ALPHANUMERIC	= "[a-zA-Z0-9]*";
     public static final String NUMERIC 		= "[0-9]*";
+    public static final String ALPHABETIC = "[a-zA-Z]*";
 
 }
