c1039157a982ef6348ca3df9d2c38a996876dc99
ryan.t.carlson@ccci.org <ryan.t.carlson@ccci.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-02-10 21:33:13 +0000

    CCP-67 swapped what are prod and failover dbs..
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@6141 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/testutils/persistence/SeamHibernatePersistenceTest.java
+++ src/main/java/org/ccci/testutils/persistence/SeamHibernatePersistenceTest.java
@@ -32,23 +32,23 @@ public abstract class SeamHibernatePersistenceTest extends SeamPersistenceTest
 	        properties.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
 	        properties.put("hibernate.connection.provider_class", "org.ccci.ccp.failover.FailoverDatasourceConnectionProvider");
 	        properties.put("hibernate.connection.driver_class", "oracle.jdbc.xa.client.OracleXADataSource");
-	        properties.put("hibernate.connection.username", "nkopp");
-	        properties.put("hibernate.connection.password", "nkopp");
-	        properties.put("hibernate.connection.url", "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS_LIST =(LOAD_BALANCE = YES)(FAILOVER = ON)(ADDRESS = (PROTOCOL = TCP)(HOST = harta9210v)(PORT = 1521))(ADDRESS = (PROTOCOL = TCP)(HOST = harta9310v)(PORT = 1521)))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = psdev.ccci.org)(FAILOVER_MODE =(TYPE = SELECT)(METHOD = BASIC)(RETRIES = 20)(DELAY= 1))))");
+	        properties.put("hibernate.connection.username", "dsstest");
+	        properties.put("hibernate.connection.password", "dsstest");
+	        properties.put("hibernate.connection.url",  "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = harta9210v)(PORT = 1521)))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = crmdapp.ccci.org)(FAILOVER_MODE=(TYPE = SELECT)(METHOD = BASIC)(RETRIES = 20)(DELAY= 1))))");
 //	        properties.put("hibernate.hbm2ddl.auto", "create-drop");
 	        properties.put("hibernate.show_sql", isShowSql());
 	        properties.put("hibernate.format_sql", "true");
 	        properties.put("hibernate.jdbc.batch_size", "0");
 	        properties.put("hibernate.query.jpaql_strict_compliance", "true");
 	        properties.put("hibernate.validator.autoregister_listeners", false);
-	        
+	   
 	        //failover custom properties
 	        properties.put("org.ccci.failover.dialect", "org.hibernate.dialect.Oracle10gDialect");
-	        properties.put("org.ccci.failover.url", ":oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = harta9210v)(PORT = 1521)))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = crmdapp.ccci.org)(FAILOVER_MODE=(TYPE = SELECT)(METHOD = BASIC)(RETRIES = 20)(DELAY= 1))))");
+	        properties.put("org.ccci.failover.url", "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS_LIST =(LOAD_BALANCE = YES)(FAILOVER = ON)(ADDRESS = (PROTOCOL = TCP)(HOST = harta9210v)(PORT = 1521))(ADDRESS = (PROTOCOL = TCP)(HOST = harta9310v)(PORT = 1521)))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = psdev.ccci.org)(FAILOVER_MODE =(TYPE = SELECT)(METHOD = BASIC)(RETRIES = 20)(DELAY= 1))))");
 	        properties.put("org.ccci.failover.driver_class", "oracle.jdbc.xa.client.OracleXADataSource");
 	        properties.put("org.ccci.failover.pool_size", "10");
-	        properties.put("org.ccci.failover.username", "dsstest");
-	        properties.put("org.ccci.failover.password", "dsstest");
+	        properties.put("org.ccci.failover.username", "nkopp");
+	        properties.put("org.ccci.failover.password", "nkopp");
 //	        properties.put("org.ccci.failover.autocommit", new Boolean(false));
 	        
 	        return Preconditions.checkNotNull(new HibernatePersistence().createEntityManagerFactory(getPersistenceUnitName(), properties), "unable to create hibernate persistence unit");
