e9c46fc94c80ffba82559664a1170214e07cfc1b
lee.braddock@ccci.org <lee.braddock@ccci.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-02-18 20:33:57 +0000

    Reading properties based on specified property source semantics. Deleted extraneous ApplicationProperties In support of: Jira CCP-62
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@6181 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/properties/Properties.java
+++ src/main/java/org/ccci/util/properties/Properties.java
@@ -6,41 +6,60 @@ import java.io.FileInputStream;
 /**
  * Properties class extended to include support for reading in properties files.
  * 
+ * Loads properties from properties source, depending on specified properties
+ * source semantics - class resource is default.
+ * 
  * @author Lee Braddock
  */
 public class Properties extends java.util.Properties {
 
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Reads the specified properties file into this class.
-	 */
-	public void getProperties(String propertiesFileName)
+	public enum PropertySourceSemantics {
+		CLASS_RESOURCE, FILENAME
+	}
+
+	public Properties(String classResource) throws RuntimeException {
+		this(classResource, PropertySourceSemantics.CLASS_RESOURCE);
+	}
+
+	public Properties(String propertiesSource,
+			PropertySourceSemantics propertyType) throws RuntimeException {
+		if (propertyType == PropertySourceSemantics.FILENAME)
+			getPropertiesFromFilename(propertiesSource);
+		else
+			getPropertiesFromClassResource(propertiesSource);
+	}
+
+	private void getPropertiesFromClassResource(String classResource) {
+		try {
+			loadFromXML(new Properties(classResource).getClass()
+					.getClassLoader().getResourceAsStream(classResource));
+		} catch (Exception e) {
+			throw new RuntimeException("Could not load class resource "
+					+ classResource, e);
+		}
+	}
+
+	private void getPropertiesFromFilename(String fileName)
 			throws RuntimeException {
 		FileInputStream fstream = null;
 		DataInputStream in = null;
 
 		try {
-			// find the file with the class loader
-			loadFromXML(new Properties().getClass().getClassLoader()
-					.getResourceAsStream(propertiesFileName));
+			fstream = new FileInputStream(fileName);
+			in = new DataInputStream(fstream);
+			loadFromXML(in);
 		} catch (Exception e) {
-			// find the file from the 'current directory' or absolute path name
+			throw new RuntimeException("Could not load properties file "
+					+ fileName, e);
+		} finally {
 			try {
-				fstream = new FileInputStream(propertiesFileName);
-				in = new DataInputStream(fstream);
-				loadFromXML(in);
-			} catch (Exception e2) {
-				throw new RuntimeException("Could not load properties file "
-						+ propertiesFileName, e2);
-			} finally {
-				try {
-					if (fstream != null)
-						fstream.close();
-					if (in != null)
-						in.close();
-				} catch (Exception e3) {
-				}
+				if (fstream != null)
+					fstream.close();
+				if (in != null)
+					in.close();
+			} catch (Exception exception) {
 			}
 		}
 	}
