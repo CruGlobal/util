496dd36dbff14e8fb2e7cca2fb4ef2f3dc417dad
lee.braddock@ccci.org <lee.braddock@ccci.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-03-19 18:10:02 +0000

    Providing support for source format specification.
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@6312 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/properties/Properties.java
+++ src/main/java/org/ccci/util/properties/Properties.java
@@ -2,6 +2,7 @@ package org.ccci.util.properties;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 /**
  * Properties class extended to include support for reading in properties files.
@@ -19,37 +20,64 @@ public class Properties extends java.util.Properties {
 		CLASS_RESOURCE, FILENAME
 	}
 
+	public enum SourceFormat {
+		KEYPAIR, XML
+	}
+
 	public Properties(String classResource) throws RuntimeException {
-		this(classResource, PropertySourceSemantics.CLASS_RESOURCE);
+		this(classResource, PropertySourceSemantics.CLASS_RESOURCE,
+				SourceFormat.XML);
+	}
+
+	public Properties(String classResource,
+			PropertySourceSemantics propertySourceSemantics)
+			throws RuntimeException {
+		this(classResource, propertySourceSemantics, SourceFormat.XML);
 	}
 
 	public Properties(String propertiesSource,
-			PropertySourceSemantics propertyType) throws RuntimeException {
-		if (propertyType == PropertySourceSemantics.FILENAME)
-			getPropertiesFromFilename(propertiesSource);
+			PropertySourceSemantics propertySourceSemantic,
+			SourceFormat sourceFormat) throws RuntimeException {
+		if (propertySourceSemantic == PropertySourceSemantics.FILENAME)
+			getPropertiesFromFilename(propertiesSource, sourceFormat);
 		else
-			getPropertiesFromClassResource(propertiesSource);
+			getPropertiesFromClassResource(propertiesSource, sourceFormat);
 	}
 
-	private void getPropertiesFromClassResource(String classResource) {
+	private void getPropertiesFromClassResource(String classResource,
+			SourceFormat sourceFormat) {
+		InputStream in = null;
 		try {
-			loadFromXML(Properties.class.getClassLoader().getResourceAsStream(
-					classResource));
+			in = Properties.class.getClassLoader().getResourceAsStream(
+					classResource);
+			if (sourceFormat == SourceFormat.XML)
+				loadFromXML(in);
+			else
+				load(in);
 		} catch (Exception e) {
 			throw new RuntimeException("Could not load class resource "
 					+ classResource, e);
+		} finally {
+			try {
+				if (in != null)
+					in.close();
+			} catch (Exception e) {
+			}
 		}
 	}
 
-	private void getPropertiesFromFilename(String fileName)
-			throws RuntimeException {
+	private void getPropertiesFromFilename(String fileName,
+			SourceFormat sourceFormat) throws RuntimeException {
 		FileInputStream fstream = null;
 		DataInputStream in = null;
 
 		try {
 			fstream = new FileInputStream(fileName);
 			in = new DataInputStream(fstream);
-			loadFromXML(in);
+			if (sourceFormat == SourceFormat.XML)
+				loadFromXML(in);
+			else
+				load(in);
 		} catch (Exception e) {
 			throw new RuntimeException("Could not load properties file "
 					+ fileName, e);
