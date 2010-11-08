bed5378b1741880ab41fda4e204b14371f6962fe
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-11-08 14:44:08 +0000

    allow counting starting at 0, or any other initial value
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@7735 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/util/Counter.java
+++ src/main/java/org/ccci/util/Counter.java
@@ -6,15 +6,40 @@ import org.ccci.annotations.NotThreadSafe;
 public class Counter
 {
 
-    private int counter = 0;
+    private int counter;
+    private final int initialValue;
     
+    
+    /**
+     * Creates a counter with the given initial value.  This value will
+     * be the first number returned by {@link #next()}.
+     */
+    public static Counter startingAt(int initialValue)
+    {
+        return new Counter(initialValue);
+    }
+
+    private Counter(int initialValue)
+    {
+        this.initialValue = initialValue;
+        this.counter = initialValue;
+    }
+    
+    /**
+     * Creates a counter whose first value is 1
+     */
+    public Counter()
+    {
+        this(1);
+    }
+
     public int next()
     {
-        return ++counter;
+        return counter++;
     }
 
     public void reset()
     {
-        counter = 0;
+        counter = initialValue;
     }
 }
