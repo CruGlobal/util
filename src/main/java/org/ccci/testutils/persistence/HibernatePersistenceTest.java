0cc3f6bf3bd50f104d39fdfa3a92388623f33970
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-11-24 22:14:53 +0000

    use h2, not hsqldb
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/util@7816 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/testutils/persistence/HibernatePersistenceTest.java
+++ src/main/java/org/ccci/testutils/persistence/HibernatePersistenceTest.java
@@ -2,28 +2,25 @@ package org.ccci.testutils.persistence;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.spi.PersistenceUnitTransactionType;
 
+import org.hibernate.dialect.H2Dialect;
 import org.hibernate.ejb.HibernatePersistence;
 
 import com.google.common.base.Preconditions;
 
 
 /**
- * A simple helper for tests which need a live entityManager.
- * A new entityManager and transaction is created for each test;
+ * A simple helper for tests which need a live {@link EntityManager}.
+ * A new {@link EntityManager} and transaction is created for each test;
  * after the test the transaction is rolled back.
  * 
- * Uses Hibernate's JPA implementation and an in-memory HSQL
+ * Uses Hibernate's JPA implementation and an in-memory H2
  * database.
  * 
  * Subclasses may use jUnit or testNG
- * 
- * Picks up persistent classes in eTimesheet-ejb/bin.
- * The path is hardcoded.  
- * See eTimesheet-test/ejbTests/META-INF/persistence.xml for comments
- * on configuration.
  * 
  * @author Matt Drees
  *
@@ -41,16 +38,14 @@ public abstract class HibernatePersistenceTest extends PersistenceTest
         properties.put("javax.persistence.transactionType",
             PersistenceUnitTransactionType.RESOURCE_LOCAL.name());
         
-        //note: this is not always sufficient; sometimes eclipse-link will still take the persistence unit during the
-        //persistence provider scan
         properties.put("javax.persistence.provider", org.hibernate.ejb.HibernatePersistence.class.getName());
         
-        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-        properties.put("hibernate.connection.provider_class", "org.hibernate.connection.DriverManagerConnectionProvider");
-        properties.put("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
+        properties.put("hibernate.dialect", H2Dialect.class.getName());
+        properties.put("hibernate.connection.provider_class", org.hibernate.connection.DriverManagerConnectionProvider.class.getName());
+        properties.put("hibernate.connection.driver_class", org.h2.Driver.class.getName() );
         properties.put("hibernate.connection.username", "sa");
         properties.put("hibernate.connection.password", "");
-        properties.put("hibernate.connection.url", "jdbc:hsqldb:.");
+        properties.put("hibernate.connection.url", "jdbc:h2:mem:hibernatePersistenceTest;DB_CLOSE_DELAY=-1;MVCC=TRUE");
         properties.put("hibernate.hbm2ddl.auto", "create-drop");
         properties.put("hibernate.show_sql", isShowSql());
         properties.put("hibernate.format_sql", "true");
@@ -61,7 +56,9 @@ public abstract class HibernatePersistenceTest extends PersistenceTest
         properties.put("hibernate.validator.autoregister_listeners", false);
         properties.put("javax.persistence.validation.mode", "none");
         
-        return Preconditions.checkNotNull(new HibernatePersistence().createEntityManagerFactory(getPersistenceUnitName(), properties), 
+        return Preconditions.checkNotNull(
+            new HibernatePersistence()
+                .createEntityManagerFactory(getPersistenceUnitName(), properties), 
             "unable to create hibernate persistence unit '" + getPersistenceUnitName() + 
             "'- this generally means that a persistence.xml file that defines this persistence unit is not on the classpath." +
             " You may need to manually run a build command to create or refresh the test resources.");
